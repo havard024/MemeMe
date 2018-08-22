45e9c2d827cb3fdbe06d78e390a86b9ae3b06eaa
havard <havardnoren@gmail.com>
2018-08-22 15:55:41 +0530

    feat: add design elements without logic

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configureUI() {
+        cameraButton.isEnabled = false
+        shareButton.isEnabled = false
+        cancelButton.isEnabled = false
+        // TODO: Figure out how to hide cancelButton
     }
 
 
