e1fe3cc808c46f81daa22a7ddfae3137153bd216
havard <havardnoren@gmail.com>
2018-08-22 17:48:35 +0530

    feat: open gallery and set image view from selected image

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,12 +33,67 @@ class ViewController: UIViewController {
     }
     
     func configureUI() {
-        cameraButton.isEnabled = false
         shareButton.isEnabled = false
         cancelButton.isEnabled = false
         // TODO: Figure out how to hide cancelButton
     }
+    
+    func checkPermission() {
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus(); switch photoAuthorizationStatus {
+        case .authorized: print("Access is granted by user")
+        case .notDetermined: PHPhotoLibrary.requestAuthorization({
+            (newStatus) in print("status is \(newStatus)"); if newStatus == PHAuthorizationStatus.authorized {
+                print("success") }
+            })
+            case .restricted: print("User do not have access to photo album.")
+            case .denied: print("User has denied the permission.")
+            }
+        }
+    
+    func openGallery(sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        imagePicker.modalPresentationStyle = .popover
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.popoverPresentationController?.barButtonItem = sender
+    }
 
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus(); switch photoAuthorizationStatus {
+        case .authorized: self.openGallery(sender: sender)
+        case .notDetermined: PHPhotoLibrary.requestAuthorization({
+            (newStatus) in print("status is \(newStatus)")
+            if newStatus == PHAuthorizationStatus.authorized {
+                self.openGallery(sender: sender)
+            }
+        })
+        case .restricted: print("User do not have access to photo album.")
+        case .denied: print("User has denied the permission.")
+        }
+    }
+    
+    //MARK: - Delegates
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = chosenImage
+        } else {
+            print("Error selecting image.")
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
 
