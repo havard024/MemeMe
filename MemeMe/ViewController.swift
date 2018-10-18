9335f9f5523d91a8e80e35ce0fa1ebce78124450
havard <havardnoren@gmail.com>
2018-10-18 18:49:37 +0200

    feat: move Meme struct to separate file

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,13 +9,6 @@
 import UIKit
 import Photos
 
-struct Meme {
-    var topText: String?
-    var bottomText: String?
-    var originalImage: UIImage
-    var memedImage: UIImage
-}
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate, UITextFieldDelegate {
 
